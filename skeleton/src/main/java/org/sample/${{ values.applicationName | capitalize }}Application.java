a96965a79635f177bb3626e0ea9d8eb5ddef5ef1
Vanush Atanian <vanatanian@gmail.com>
2024-01-28 01:06:47 +0400

    reset test-changes

--- skeleton/src/main/java/org/sample/${{ values.applicationName | capitalize }}Application.java
+++ skeleton/src/main/java/org/sample/${{ values.applicationName | capitalize }}Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ${{ values.applicationBaseName | capitalize }}Application {
+public class $ {{ values.applicationBaseName | capitalize }}Application{
+
     public static void main(String[] args) {
         SpringApplication.run(${{ values.applicationBaseName | capitalize }}Application.class, args);
     }
 }
+
+// some change
