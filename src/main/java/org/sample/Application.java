20786ae594fef805d7166d04290cf01317e666a5
VanushAtanian <85105721+VanushAtanian@users.noreply.github.com>
2024-01-28 02:06:49 +0400

    Test creating PR from template

---
+++ src/main/java/org/sample/Application.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class $ {{ values.applicationBaseName | capitalize }}Application{
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstattemptApplication.class, args);
+    }
+}
